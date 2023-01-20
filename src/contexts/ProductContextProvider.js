3bedf9830986bf18f53df5a933a3e6312c092976
FairUser21 <renat.alimbekov2002@gmail.com>
2023-01-20 12:17:46 +0600

    .

--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useReducer } from "react";
 import axios from "axios";
 import { API } from "../helpers/consts";
+import { useNavigate } from "react-router-dom";
 
 export const productContext = createContext();
 
@@ -24,6 +25,7 @@ function reducer(state = INIT_STATE, action) {
 
 const ProductContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
+  const navigate = useNavigate();
 
   const getProducts = async () => {
     try {
@@ -37,10 +39,54 @@ const ProductContextProvider = ({ children }) => {
     }
   };
 
+  const addProduct = async (obj) => {
+    try {
+      await axios.post(API, obj);
+      navigate("/products");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteProduct = async (id) => {
+    try {
+      await axios.delete(`${API}/${id}`);
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getProductDetails = async (id) => {
+    try {
+      let res = await axios(`${API}/${id}`);
+      dispatch({
+        type: "GET_PRODUCT_DETAILS",
+        payload: res.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const saveEditProduct = async (id, obj) => {
+    try {
+      await axios.patch(`${API}/${id}`, obj);
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   let value = {
     products: state.products,
+    productDetails: state.productDetails,
 
     getProducts,
+    addProduct,
+    deleteProduct,
+    getProductDetails,
+    saveEditProduct,
   };
   return (
     <productContext.Provider value={value}>{children}</productContext.Provider>
