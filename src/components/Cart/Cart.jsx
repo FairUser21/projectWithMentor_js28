7619e296e31e8eae4a321bfa98d28d670fb3ac17
soltonbaev <isoltonbaev@gmail.com>
2023-01-24 22:33:53 +0600

    .

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import {Container} from '@mui/system';
+import React, {useEffect, useState} from 'react';
+import {useProduct} from '../../contexts/ProductContextProvider';
+import CartItem from './CartItem';
 
 const Cart = () => {
-  return <div></div>;
+   const cartItemIds = [2, 3, 4];
+   const [cartItems, setCartItems] = useState([
+      {
+         title: 'lycoris recoil',
+         price: 987,
+         img: 'https://static.zerochan.net/Lycoris.Recoil.full.3706694.jpg',
+         type: 'action',
+         id: 2,
+      },
+      {
+         title: 'akame ga kill',
+         price: 123,
+         img: 'https://i.pinimg.com/originals/7b/ef/90/7bef90845b56b5c4d04db30fa196fd0c.jpg',
+         type: 'fantasy',
+         id: 3,
+      },
+      {
+         title: 'monogatari',
+         price: 111,
+         img: 'https://images.alphacoders.com/244/244443.jpg',
+         type: 'daily',
+         id: 4,
+      },
+   ]);
+   const {products, getProducts, fetchByParams} = useProduct();
+   useEffect(() => {
+      getProducts();
+   }, []);
+
+   return (
+      <>
+         <h1>My Cart</h1>
+         <Container>
+            {cartItems.map(cartItem => {
+               {
+                  return <CartItem item={cartItem} key={cartItem.id} />;
+               }
+            })}
+         </Container>
+      </>
+   );
 };
 
 export default Cart;
